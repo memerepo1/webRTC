1123280585a1452b8c571b3b9cde140ffc527d74
memerepo1 <alexpar@outlook.com>
2017-07-15 12:34:58 -0700

    yellow

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express.io');
 var app = express();
 app.http().io();
 var PORT = 3000;
-console.log('server started');
+console.log('server started on port ' + PORT);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,26 +10,30 @@ app.get('/', function(req, res){
 	res.render('index.ejs');
 });
 
+app.listen(PORT);
+
 app.io.route('ready', function(req) {
 	req.io.join(req.data.chat_room);
 	req.io.join(req.data.signal_room);
 	app.io.room(req.data).broadcast('announce', {
 		message: 'New client in the ' + req.data + ' room.'
-	});
-});
+	})
+})
 
 app.io.route('send', function(req) {
-	app.io.room(req.data.room).broadcast('message', {
-		message: req.data.message,
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
 		author: req.data.author
-	});
-});
+    });
+})
 
 app.io.route('signal', function(req) {
+	//Note the use of req here for broadcasting so only the sender doesn't receive their own messages
 	req.io.room(req.data.room).broadcast('signaling_message', {
-		type: req.data.type,
+        type: req.data.type,
 		message: req.data.message
-	});
-});
+    });
+})
 
-app.listen(process.env.PORT || 5000)
+
+
